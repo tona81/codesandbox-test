6be21f7fb929750fe2a35635dd08c3ac7e406db8
tona81 <123795719+tona81@users.noreply.github.com>
2023-04-28 16:33:20 +0900

    sec03

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import { useState } from "react";
+import { ChildArea } from "./ChildArea";
+import { useState, useCallback, useMemo } from "react";
 import "./styles.css";
 
 export default function App() {
   console.log("App");
-  const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+  const [open, setOpen] = useState(false);
 
-  const onClickCountUp = () => {
-    setCount(count + 1);
-  };
+  const onChangeText = (e) => setText(e.target.value);
 
-  return <div className="App">input</div>;
+  const onClickOpen = () => setOpen(!open);
+
+  const onClickClose = useCallback(() => setOpen(false), [setOpen]);
+
+  const temp = useMemo(() => 1 + 3, []);
+  console.log(temp);
+
+  return (
+    <div className="App">
+      <input value={text} onChange={onChangeText} />
+      <br />
+      <br />
+      <button onClick={onClickOpen}>表示</button>
+      <ChildArea open={open} onClickClose={onClickClose} />
+    </div>
+  );
 }
